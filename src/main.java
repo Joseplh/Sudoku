ffb8064e824501a5d088ddc36d874161ee116544
Joseplh <joemax94@comcast.net>
2020-09-16 16:05:02 -0500

    Added todo list
    Added todo list in the main for the program and started to build the puzzle reader.

---
+++ src/main.java
@@ -0,0 +1,50 @@
+/**Developed by Joseph Maxwell
+ * Sudoku is a common puzzle in newspapers across the US.  It is a 9x9 grid of numbers from 1-9.
+ * The grid is broken further down into 9 sections of 3x3 block.  To solve the puzzle, the user
+ * must enter a number into every grid position and not violate any of the three rules.
+ * -There must be only one of every number in each 3x3 block.
+ * -There must be only one of every number in each horizontal row
+ * -There must be only one of every number in each vertical column
+ * @author Joseph Maxwell
+ **/
+public class main {
+
+	public static void main(String[] args) {
+		/* TODO Open a display for the program with a menu
+		* The menu has a display for the current sudoku puzzle and three buttons to the side
+		* Menu screen offers 3 options: "Enter New Puzzle", "Solve Existing puzzle", and Exit 
+		* "Enter New Puzzle" replaces menu with a new screen that has a text box and two buttons
+		* The text box allows the user to enter the address to a .csv used to store a puzzle
+		* The two buttons are to confirm and cancel, confirm has the program read into memory the puzzle
+		* */
+		
+		/* TODO Method to take in a String and read into memory a .csv file formatted to fit a sudoku board
+		 * Needs filter for the String to check for a valid file
+		 * Needs filter to check .csv file contains a valid sudoku problem
+		 * */
+		
+		/* TODO Method to hold a 9x9 grid of cells that store it's current value, possible legal values, and number of legal values
+		 * */
+		
+		/* TODO Method to loop through the 9x9 grid and
+		 * */
+		
+		/* TODO Method to check vertical column for invalid numbers
+		 * */
+		
+		/* TODO Method to check horizontal column for invalid numbers
+		 * */
+		
+		/* TODO Method to check host block for invalid numbers
+		 * */
+		
+		/* TODO Method to resolve logical leaps in puzzle.  
+		 * An example case would be where there are no cells with 1 possibility to lock-in.
+		 * Here the program should call this method to take one cell with 2 possibilities and attempt to solve one with the first value
+		 * Returns true/false
+		 * false has method repeat with other value.
+		 * true inserts correct value in cell.
+		 * */
+	}
+
+}
---
+++ src/readPuzzle.java
@@ -0,0 +1,44 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class readPuzzle {
+	private int counter = 0;
+	private char nextChar;
+	protected boolean readPuzzle(){
+		try{
+			File inputFile = new File("FileName.txt");
+			Scanner fileReader = new Scanner(inputFile);
+			while(fileReader.hasNext())
+			switch (fileReader.next().charAt(0)){
+			case ','://move to next cell
+				break;
+			case '\n'://reset counter to 0 and continue to next line
+				break;
+			case '1':
+				break;
+			case '2':
+				break;
+			case '3':
+				break;
+			case '4':
+				break;
+			case '5':
+				break;
+			case '6':
+				break;
+			case '7':
+				break;
+			case '8':
+				break;
+			case '9':
+				break;
+			default: return false;//there is an abnormality in the format of the csv file and is rejected
+			}
+			
+		}catch (FileNotFoundException  err){
+			err.printStackTrace();
+		}
+		return false;//this return statement should only occur if there is a critical failure
+	}
+}
